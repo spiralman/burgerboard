db741c4f1186ac83ae569e306f71b339deaef17c
Thomas Stephens <spiralman@gmail.com>
2013-09-22 15:19:28 -0500

    Initial commit

---
+++ src/burgerboard/handler.clj
@@ -0,0 +1,13 @@
+(ns burgerboard.handler
+  (:use compojure.core)
+  (:require [compojure.handler :as handler]
+            [compojure.route :as route]))
+
+(defroutes app-routes
+  (GET "/" [] "Hello World")
+  (POST "/login" [] "Login")
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(def app
+  (handler/site app-routes))
---
+++ test/burgerboard/test/handler.clj
@@ -0,0 +1,27 @@
+(ns burgerboard.test.handler
+  (:use clojure.test
+        [clojure.data.json :as json]
+        ring.mock.request
+        burgerboard.handler))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (request :get "/"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "Hello World"))))
+
+  (testing "login route"
+    (let [response
+          (app
+           (->
+            (request :post "/login")
+            (content-type "application/json")
+            (body (json/write-str {:foo "bar"}))
+            ))]
+      (is (= (:status response) 200))
+      )
+    )
+  
+  (testing "not-found route"
+    (let [response (app (request :get "/invalid"))]
+      (is (= (:status response) 404)))))
