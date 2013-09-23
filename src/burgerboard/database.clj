69f83df9dca8bb5e05f0e64bccbfb4f9ef178495
Thomas Stephens <spiralman@gmail.com>
2013-09-22 22:30:09 -0500

    basic users

--- src/burgerboard/database.clj
+++ src/burgerboard/database.clj
@@ -1,11 +1,39 @@
 (ns burgerboard.database
-  (:use korma.db)
-  (:require [clojure.java.jdbc :as jdbc])
+  (:use korma.db
+        korma.core)
+  (:require [clojure.java.jdbc :as jdbc]
+            [crypto.password.bcrypt :as password])
   )
 
 (defn create-schema []
   (jdbc/create-table
    :users
-   [:username "varchar(32)" "PRIMARY KEY"]
+   [:username "varchar" "PRIMARY KEY"]
+   [:password "varchar"]
    )
+  )
+
+(defentity users
+  (pk :username)
+  (entity-fields :password)
+  )
+
+(defn create-user [username password]
+  (insert users
+          (values {:username username :password (password/encrypt password)})))
+
+(defn login-valid [username password]
+  (if-let [user (first
+                 (exec
+                  (->
+                   (select* users)
+                   (where {:username username})))
+                 )]
+    (password/check
+     password
+     (:password
+      user
+      )
+     )
+    )
   )
--- test/burgerboard/test/database_test.clj
+++ test/burgerboard/test/database_test.clj
@@ -1,13 +1,31 @@
 (ns burgerboard.test.database-test
   (:use clojure.test
         burgerboard.database
-        korma.db)
+        korma.db
+        korma.core)
   (:require
    [clojure.java.jdbc :as jdbc]
    clojure.pprint))
 
 (defn get-table [name tables]
   (first (filter (fn [table] (= name (:table_name table))) tables)))
+
+(defn get-column [name columns]
+  (first (filter (fn [column] (= name (:column_name column))) columns)))
+
+(defn get-tables []
+  (jdbc/resultset-seq
+   (-> (jdbc/connection)
+       (.getMetaData)
+       (.getTables nil nil nil (into-array ["TABLE"]))))
+  )
+
+(defn get-columns [table]
+  (jdbc/resultset-seq
+   (-> (jdbc/connection)
+       (.getMetaData)
+       (.getColumns nil nil table nil)))
+  )
 
 (def testing-db-spec
   (sqlite3
@@ -17,19 +35,36 @@
 (defdb testing-db
   testing-db-spec)
 
+(defn with-database [f]
+  (jdbc/with-connection testing-db-spec
+    (create-schema)
+    (f)
+    )
+  )
+
+(use-fixtures :each with-database)
+
 (deftest test-create-schema
   (testing "DB schema is created"
-    (jdbc/with-connection testing-db-spec
-      (create-schema)
+    (let [tables (get-tables)]
+      (is (not (nil? (get-table "users" tables))))
 
-      (let [tables 
-            (jdbc/resultset-seq
-             (-> (jdbc/connection)
-                 (.getMetaData)
-                 (.getTables nil nil nil (into-array ["TABLE" "VIEW"]))))]
-        (is (not (nil? (get-table "users" tables))))
+      (let [users-cols (get-columns "users")]
+        (is (= "VARCHAR" (:type_name (get-column "username" users-cols))))
+        (is (= "VARCHAR" (:type_name (get-column "password" users-cols))))
         )
       )
+    )
+  )
+
+(deftest test-users
+  (testing "User creation and password verification"
+    (create-user "username" "password")
+
+    (is (not (= "password" (:password (first (exec (select* users)))))))
+    (is (login-valid "username" "password"))
+    (is (not (login-valid "username" "other password")))
+    (is (not (login-valid "other-user" "password")))
     )
   )
 
