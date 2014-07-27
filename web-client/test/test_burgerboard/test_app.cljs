4b8927e180f3696e518c5fad2326c437801b4607
Thomas Stephens <spiralman@gmail.com>
2014-07-26 21:23:11 -0500

    Moving assertion code out to its own files, so it's easier to write a custom assert-expr method, for better error reporting

--- web-client/test/test_burgerboard/test_app.cljs
+++ web-client/test/test_burgerboard/test_app.cljs
@@ -1,81 +1,12 @@
-(ns burgerboard-test.test-app
+(ns test-burgerboard.test-app
   (:require-macros [cemerick.cljs.test
                     :refer (is deftest with-test testing test-var)]
+                   [test-burgerboard.huh]
                    )
-  (:require [cemerick.cljs.test :as t]
-            [burgerboard-web.app :as app]
-            [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]
-            [clojure.string :refer [upper-case]]
-            )
-  )
-
-(defn -instrument [sub-component cursor _]
-  {:sub-component sub-component
-   :cursor cursor
-   }
-  )
-
-(defn rendered [component state & tests]
-  (binding [om/*instrument* -instrument]
-    (let [rendered-comp (.render (om/build* component state {}))]
-      ((apply every-pred tests) rendered-comp)
-      )
-    )
-  )
-
-(defn tag [tag-name & tests]
-  (fn [component]
-    (and
-     (= (upper-case tag-name) (.-tagName component))
-     (if (empty? tests)
-       true
-       ((apply every-pred tests) component)
-       )
-     )
-    )
-  )
-
-(defn containing [& tests]
-  (fn [component]
-    (let [children (js->clj (.. component -props -children))]
-      (if (sequential? children)
-        (and
-         (= (count tests) (count children))
-         (every? true?
-                 (map (fn [pred child] (pred child))
-                      tests children
-                      )
-                 )
-         )
-        (and
-         (= 1 (count tests))
-         ((nth tests 0) children) ;; Children is actually just one child
-         )
-        )
-      )
-    )
-  )
-
-(defn text [text]
-  (fn [component]
-    (= text component)
-    )
-  )
-
-(defn sub-component [sub-component cursor]
-  (fn [component]
-    (= {:sub-component sub-component
-        :cursor cursor}
-       component
-       )
-    )
-  )
-
-(defn with-class [class-name]
-  (fn [component]
-    (= class-name (.. component -props -className))
-    )
+  (:require
+   [test-burgerboard.huh :refer [rendered, tag, containing, with-class, sub-component, text]]
+   [burgerboard-web.app :as app]
+   )
   )
 
 (deftest app-contains-sub-components
@@ -124,7 +55,7 @@
                    )
                   )
              (tag "ul"
-                  (with-class "boards")
+                  (with-class "boards-wrong")
                   (containing
                    (sub-component app/board-nav {:id 1})
                    (sub-component app/board-nav {:id 2})
