b0a57beb59c4bb06baa21f35025b658063a036b8
Thomas Stephens <spiralman@gmail.com>
2014-12-10 21:23:54 -0600

    Forcing leiningen 2 for Heroku

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject burgerboard "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
                  [ring/ring-jetty-adapter "1.2.2"]
